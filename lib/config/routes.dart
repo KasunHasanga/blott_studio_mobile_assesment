06678daac13d2e867f9410ed0237fd8c4ef38c52
kasun Hasanga <Kasunhasanga96@gmail.com>
2024-11-05 00:35:07 +0530

    remove unused code

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -7,8 +7,6 @@ import '../features/onboarding/onboard_binding.dart';
 import '../features/onboarding/presentation/pages/onboarding_page.dart';
 import '../features/onboarding/presentation/pages/sign_in_page.dart';
 import '../features/onboarding/presentation/pages/splash.dart';
-import '../features/profile/presentation/pages/profile_page.dart';
-import '../features/profile/profile_binding.dart';
 import '../features/web_view/presentation/pages/web_view_page.dart';
 import '../features/web_view/web_view_binding.dart';
 
@@ -30,19 +28,11 @@ class Routers {
         page: () => const OnboardingPage(),
         bindings: [OnBoardingBinding()]),
 
-
     //Home Page
     GetPage(
         name: HomePage.routeName,
         page: () => const HomePage(),
         bindings: [HomePageBinding()]),
-
-    //profile
-    GetPage(
-        name: ProfilePage.routeName,
-        page: () => const ProfilePage(),
-        bindings: [ProfileBinding()]),
-
 
     //Web View page
     GetPage(
