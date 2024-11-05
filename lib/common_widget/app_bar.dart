dcb43b93ecd1d3871396e45d014c79f4cee05401
kasun Hasanga <Kasunhasanga96@gmail.com>
2024-11-05 21:05:27 +0530

    app bar widget code refactoring

--- lib/common_widget/app_bar.dart
+++ lib/common_widget/app_bar.dart
@@ -5,18 +5,14 @@ import '../config/fonts.dart';
 class MainAppBar extends StatelessWidget {
   const MainAppBar({
     super.key,
-    this.excludeHeader = false,
     this.showBack = false,
-    this.otherAction=const [],
-
+    this.otherAction = const [],
     required this.title,
   });
-  final bool excludeHeader;
 
   final bool showBack;
   final String title;
   final List<Widget> otherAction;
-
 
   @override
   Widget build(BuildContext context) {
@@ -24,13 +20,14 @@ class MainAppBar extends StatelessWidget {
       elevation: 0,
       centerTitle: false,
       surfaceTintColor: Colors.transparent,
-      title: Text(title,  style: AppFonts.styleWithRobotoMediumText(
-          color:Theme.of(context).colorScheme.onSurface,fSize: FontSizeValue.fontSize16),),
-actions: otherAction,
-
+      title: Text(
+        title,
+        style: AppFonts.styleWithRobotoMediumText(
+            color: Theme.of(context).colorScheme.onSurface,
+            fSize: FontSizeValue.fontSize16),
+      ),
+      actions: otherAction,
       backgroundColor: Theme.of(context).colorScheme.background,
-    );}
-
-
+    );
   }
-
+}
